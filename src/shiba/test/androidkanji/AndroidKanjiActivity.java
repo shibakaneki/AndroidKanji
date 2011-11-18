b4f8e202cdc102564acff3eb4928b5aafe6ca318
shibakaneki <didier@sente.ch>
2011-11-18 13:59:28 +0100

    Initial commit

---
+++ src/shiba/test/androidkanji/AndroidKanjiActivity.java
@@ -0,0 +1,13 @@
+package shiba.test.androidkanji;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidKanjiActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
