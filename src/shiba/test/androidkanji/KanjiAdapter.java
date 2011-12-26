c88403e06c6df95e839f7852e548db5af9f3ee65
shibakaneki <ShibaKaneki@MacBook-Pro-de-Shiba.local>
2011-12-26 21:05:57 +0100

    Updated some files

--- src/shiba/test/androidkanji/KanjiAdapter.java
+++ src/shiba/test/androidkanji/KanjiAdapter.java
@@ -1,12 +1,13 @@
 package shiba.test.androidkanji;
 
-import android.R.integer;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -55,12 +56,20 @@ public class KanjiAdapter extends BaseAdapter {
 		// Get the value at the given position
 		int index = _kanjiCursor.getColumnIndex(KanjiDBHelper.KEY_ID);
 		TextView kanjiTV = (TextView)layoutItem.findViewById(R.id.text1);
+		ImageView favStar = (ImageView)layoutItem.findViewById(R.id.favoriteStar);
 		
 		// Convert the text value in UTF8 character
 		String value = _kanjiCursor.getString(index);
 		int iVal = Integer.parseInt(value);
 		
+		// Set the kanji value
 		kanjiTV.setText(TextTools.codeToKanji(iVal));			
+		
+		// Now we will add the favorite star
+		//	TODO : For each kanji, check if it is part of the favorites or not, then display the empty star or the full one
+		//			depending on the favorite entry.
+		Drawable fav = _context.getResources().getDrawable(R.drawable.favfull);
+		favStar.setImageDrawable(fav);
 		
 		return layoutItem;
 	}
